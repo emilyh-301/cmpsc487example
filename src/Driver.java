66bfb4c07cf61e170128462fbce1c94ec7a337ca
emilyh-301 <36495239+emilyh-301@users.noreply.github.com>
2020-09-04 11:56:58 -0400

    first commit

---
+++ src/Driver.java
@@ -0,0 +1,15 @@
+public class Driver {
+
+    public static void main(String[] args){
+
+        System.out.println("Hello software engineering student");
+
+        String favAnimal = favoriteAnimal();
+    }
+
+    public static String favoriteAnimal(){
+        // TODO: ask the user what their favorite animal is and return their answer
+        return "";
+    }
+
+}
